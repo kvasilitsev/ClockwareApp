e9de079e31683969a0c3ec0bfa053557b606533e
kvasilitsev <k.vasylitsev@gmail.com>
2022-12-20 22:14:36 -0400

    debugging heroku deploy v1

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
+// require('dotenv').config({path: './.env'});
+// const express = require("express");
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser');
+// const log4js = require("./server/logger");
+// const masterRouter = require('./server/routes/master.routes');
+// const cityRouter = require('./server/routes/city.routes');
+// const clockRouter = require('./server/routes/clock.routes');
+// const userRouter = require('./server/routes/user.routes');
+// const orderRouter = require('./server/routes/order.routes');
+// const emailRouter = require('./server/routes/email.routes');
+// const errorMiddleware = require('./server/middlewares/error.middleware');
+// const path = require('path');
+// const app = express();
+
+
+// const logger = log4js.getLogger("./server/logs/clockwiseLog");
+// const PORT = process.env.PORT || 3001;
+
+
+// app.use(express.static(path.join(__dirname, 'client/build'))); //run static build
+// // app.get('/*', function(req,res) {
+// //   res.sendFile(path.join(__dirname, 'client/build', 'index.html')); //route with static build
+// // });
+
+// if (process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }
+
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use('/api', cors(corsOptions), masterRouter);
+// app.use('/api', cityRouter);
+// app.use('/api', clockRouter);
+// app.use('/api', cors(corsOptions), userRouter);
+// app.use('/api', cors(corsOptions), orderRouter);
+// app.use('/api', cors(corsOptions), emailRouter);
+// app.use(errorMiddleware);
+
+  
+// app.listen(PORT, () => {  
+//   logger.info(`Server listening on ${PORT}`);  
+// });
+
 require('dotenv').config({path: './.env'});
 const express = require("express");
-const cors = require('cors')
-const cookieParser = require('cookie-parser');
 const log4js = require("./server/logger");
-const masterRouter = require('./server/routes/master.routes');
-const cityRouter = require('./server/routes/city.routes');
 const clockRouter = require('./server/routes/clock.routes');
-const userRouter = require('./server/routes/user.routes');
-const orderRouter = require('./server/routes/order.routes');
-const emailRouter = require('./server/routes/email.routes');
-const errorMiddleware = require('./server/middlewares/error.middleware');
 const path = require('path');
 const app = express();
-
-
 const logger = log4js.getLogger("./server/logs/clockwiseLog");
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.static(path.join(__dirname, 'client/build'))); //run static build
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html')); //route with static build
-// });
 
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -30,23 +71,8 @@ if (process.env.NODE_ENV === 'production'){
   });
 }
 
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true
-}
-
-
 app.use(express.json());
-app.use(cookieParser());
-
-app.use('/api', cors(corsOptions), masterRouter);
-app.use('/api', cityRouter);
 app.use('/api', clockRouter);
-app.use('/api', cors(corsOptions), userRouter);
-app.use('/api', cors(corsOptions), orderRouter);
-app.use('/api', cors(corsOptions), emailRouter);
-app.use(errorMiddleware);
 
   
 app.listen(PORT, () => {  
--- server/routes/clock.routes.js
+++ server/routes/clock.routes.js
@@ -1,11 +1,14 @@
 const Router = require('express');
+const log4js = require('../logger');
+const logger = log4js.getLogger("clockwiseLog");
 const router = new Router();
 const clockController = require("../controllers/clock.controller")
 
-router.post("/createClock", clockController.createClock);
+logger.info('clocks router');
+// router.post("/createClock", clockController.createClock);
 router.get("/getClocks", clockController.getClocks);
-router.put("/updateClock", clockController.updateClock);
-router.delete("/deleteClock/:id", clockController.deleteClock);
-router.get("/getRepairDurationByClockId/:id", clockController.getRepairDurationByClockId);
+// router.put("/updateClock", clockController.updateClock);
+// router.delete("/deleteClock/:id", clockController.deleteClock);
+// router.get("/getRepairDurationByClockId/:id", clockController.getRepairDurationByClockId);
 
 module.exports = router;
