5da00786972b82e79b51dd69329a46e967f318bd
kvasilitsev <k.vasylitsev@gmail.com>
2023-03-16 21:46:39 -0300

    add active link for Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,35 @@
 import * as React from "react";
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 import { CurrentAuthContext } from '../App';
 import logout from "../utils/logoutFunction";
 
-const Header = () => {
-  const value = React.useContext(CurrentAuthContext);  
+const Header = () => {  
+const value = React.useContext(CurrentAuthContext);
   if(value) {
     return (    
       <header>   
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
+        <Container>         
           <Navbar.Brand href="/">Clockwise Clockware</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="/orders">Orders</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
-            <Nav.Link href="/masters">Masters</Nav.Link>
-            <Nav.Link href="/cities">Cities</Nav.Link>
-            <Nav.Link href="/clocks">Clocks</Nav.Link>              
+            <Nav className="me-auto"  activeKey={window.location.pathname}>
+              <Nav.Item>           
+                <Nav.Link className="link" href="/orders">Orders</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>                    
+                <Nav.Link className="link" href="/users" >Users</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>                     
+                <Nav.Link className="link" href="/masters" >Masters</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>                
+                <Nav.Link className="link" href="/cities" >Cities</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="link" href="/clocks" >Clocks</Nav.Link>              
+              </Nav.Item>   
             </Nav>
             <Nav className="ms-auto">            
               <Nav.Link onClick={()=>logout()}>Logout</Nav.Link>       
