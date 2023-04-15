b40f2b116849d3d49aaeab0f37590db7d9e2d3d8
kvasilitsev <k.vasylitsev@gmail.com>
2023-04-15 17:14:50 -0300

    add button "book another repair" in success component, remove redirect to the main page

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,16 +1,12 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
-const Success = (props) => { 
-  const navigate = useNavigate();
-  useEffect(() => {
-    setTimeout(() => {     
-      navigate('/', { replace: true });
-    }, 8000);
-  });
+const Success = () => {
   return (
-    <div>     
-      <p>Thank you for your order. You will shortly receive an order confirmation email</p>
+    <div className='success'>     
+      <p>Thank you for your order. You will shortly receive an order confirmation email.</p>
+      <Button variant='secondary' as={Link} to="/">Book another repair</Button>
     </div>
   );
 }
